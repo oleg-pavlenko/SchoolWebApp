3cdf974e211dd9cdb3238ec1d696158f9f6df643
Oleh Pavlenko <oleh.pavlenko@globallogic.com>
2021-05-15 14:04:17 +0300

    moved disciplines to homepage and added alert to display if data was submitted

--- SchoolWebApp/Controllers/HomeController.cs
+++ SchoolWebApp/Controllers/HomeController.cs
@@ -16,16 +16,65 @@ namespace SchoolWebApp.Controllers
   [Authorize]
   public class HomeController : Controller
   {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _db;
+    private readonly UserManager<Student> _userManager;
 
-    public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, UserManager<Student> userManager)
+    public HomeController(ApplicationDbContext db, UserManager<Student> userManager)
     {
-      _logger = logger;
+      _db = db;
+      _userManager = userManager;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-      return View();
+      Student student = await _db.Users
+        .Include(s => s.Disciplines)
+        .SingleOrDefaultAsync(s => s.UserName == User.Identity.Name);
+      List<Discipline> disciplines = await _db.Disciplines.ToListAsync();
+
+      List<DisciplineViewModel> selectedDisciplines = new List<DisciplineViewModel>();
+      foreach (Discipline d in disciplines)
+      {
+        DisciplineViewModel selectedDiscipline = new DisciplineViewModel
+        {
+          Discipline = d,
+          IsSelected = student.Disciplines.Contains(d)
+        };
+        selectedDisciplines.Add(selectedDiscipline);
+      }
+      StudentToDisciplinesViewModel studentToDisciplines = new StudentToDisciplinesViewModel
+      {
+        Student = student,
+        Disciplines = selectedDisciplines
+      };
+      if (TempData["Message"] != null)
+      {
+        ViewBag.Message = TempData["Message"].ToString();
+      }
+      return View(studentToDisciplines);
+    }
+
+    [HttpPost]
+    public IActionResult SelectDisciplines(StudentToDisciplinesViewModel studentDisciplines)
+    {
+      Student student = _db.Users
+          .Include(s => s.Disciplines)
+          .SingleOrDefault(s => s.Id == studentDisciplines.Student.Id);
+      student.Disciplines.Clear();
+
+      foreach (DisciplineViewModel discipline in studentDisciplines.Disciplines)
+      {
+        if (discipline.IsSelected)
+        {
+          Discipline newDiscipline = _db.Disciplines.SingleOrDefault(d => d.Id == discipline.Discipline.Id);
+          student.Disciplines.Add(newDiscipline);
+        }
+      }
+
+      _db.SaveChanges();
+      TempData["Message"] = "Changes saved";
+
+      return RedirectToAction(nameof(Index));
     }
 
     [AllowAnonymous]
